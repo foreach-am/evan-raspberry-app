b25df8412d103d7dc4549cc6c5fb7539471a27c1
rado196 <rado196b@gmail.com>
2022-06-06 18:56:24 +0400

    fixes

--- .configs/install-rc522-driver.sh
+++ .configs/install-rc522-driver.sh
@@ -16,12 +16,18 @@ sudo apt-get install -y build-essential
 
 # download and extract driver
 cd /tmp
-wget http://www.airspayce.com/mikem/bcm2835/bcm2835$DRIVER_VERSION.tar.gz
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-$DRIVER_VERSION.tar.gz
 tar -zxf bcm2835-$DRIVER_VERSION.tar.gz
 cd bcm2835-$DRIVER_VERSION
 
 # install driver
+./configure
 make
 sudo make check
 sudo make install
 sudo modprobe spi_bcm2835
+
+# cleanup
+cd ..
+rm -rf bcm2835-$DRIVER_VERSION.tar.gz
+rm -rf bcm2835-$DRIVER_VERSION
