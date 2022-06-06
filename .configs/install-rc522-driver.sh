24abcc8cb668bd86fdb30403159d263666ebf85b
rado196 <rado196b@gmail.com>
2022-06-06 17:12:50 +0400

    ...

---
+++ .configs/install-rc522-driver.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# specify driver version
+DRIVER_VERSION=1.49
+
+# update system
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo rpi-update
+sudo apt-get -y clean
+
+# install gcc
+sudo apt-get install -y gcc
+sudo apt-get install -y build-essential
+
+# download and extract driver
+cd /tmp
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835$DRIVER_VERSION.tar.gz
+tar -zxf bcm2835-$DRIVER_VERSION.tar.gz
+cd bcm2835-$DRIVER_VERSION
+
+# install driver
+make
+sudo make check
+sudo make install
+sudo modprobe spi_bcm2835
