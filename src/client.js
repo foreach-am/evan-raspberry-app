d1ae745ef4739d7da623db3a1fee30a9cbb12730
rado196 <rado196b@gmail.com>
2022-06-07 20:01:22 +0400

    log fixes

--- src/client.js
+++ src/client.js
@@ -20,8 +20,8 @@ function logParseData() {
    * @param {string} key
    * @param {*} value
    */
-  const append = function (key, value) {
-    logResult[key.padStart(20, ' ')] = value;
+  const append = function (key, value, char = ' ') {
+    logResult[key.padStart(22, char)] = value;
   };
 
   const logResult = {};
@@ -39,12 +39,13 @@ function logParseData() {
     powerKwh,
   } = state.statistic.plugs;
 
-  append('Temperature', `${temperature} C`);
+  append('Device Temperature', `${temperature} C`);
   append('HighVoltError', `${highVoltError} State`);
   append('LowVoltError', `${lowVoltError} State`);
   append('HighVoltageMeasure', `${highVoltageMeasure} V.AC`);
 
   for (let i = 1; i <= state.maxPlugsCount; ++i) {
+    append(`PLUG [${i}]`, '----------------------------------------', '-');
     append(`PlugState[${i}]`, `${plugState[i]} State`);
     append(`PowerKwH[${i}]`, `${powerKwh[i]} KW/h`);
     append(`FeedBackVolt[${i}]`, `${pilotFeedBack[i]} V`);
