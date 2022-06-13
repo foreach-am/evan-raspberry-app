8eaf2c7f26d556195aeeb073c5fe68230b75344a
rado196 <rado196b@gmail.com>
2022-06-13 14:54:02 +0400

    export fixes

--- src/utils/uuid.js
+++ src/utils/uuid.js
@@ -1,17 +1,17 @@
 const crypto = require('crypto');
 
-module.exports.native = function () {
+function native() {
   return crypto.randomUUID();
-};
+}
 
-module.exports.custom = function () {
+function custom() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (character) {
     const randomByte = (Math.random() * 16) | 0;
     const value = character === 'x' ? randomByte : (randomByte & 0x3) | 0x8;
 
     return value.toString(16).toUpperCase();
   });
-};
+}
 
 module.exports = function () {
   return native();
