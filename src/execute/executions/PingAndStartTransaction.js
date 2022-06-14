a109e78f8667560f094780118c6621bdf373bcec
rado196 <rado196b@gmail.com>
2022-06-14 18:10:44 +0400

    ....

--- src/execute/executions/PingAndStartTransaction.js
+++ src/execute/executions/PingAndStartTransaction.js
@@ -1,4 +1,5 @@
 const uuid = require('../../utils/uuid');
+const state = require('../../state');
 const ping = require('../../ping');
 
 module.exports = async function (connectorId) {
@@ -8,5 +9,5 @@ module.exports = async function (connectorId) {
     ping.StatusNotification.ErrorCodeEnum.NO_ERROR
   );
 
-  await ping.StartTransaction.execute(uuid(), connectorId);
+  await ping.StartTransaction.execute(uuid(), connectorId, state.state.plugs.idTags[connectorId]);
 };
