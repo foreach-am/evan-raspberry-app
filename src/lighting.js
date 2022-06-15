1e9f57d24479a26f014375000049eeb990b3bbec
rado196 <rado196b@gmail.com>
2022-06-15 19:50:57 +0400

    fix

--- src/lighting.js
+++ src/lighting.js
@@ -3,4 +3,4 @@ const { LightController } = require('./libraries/LightController');
 // start lighting from 18:00 to next day 09:30
 // check every 30 seconds
 
-LightController.register(30, '18:00', '09:30');
+LightController.start(30, '18:00', '09:30');
