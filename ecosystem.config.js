b82c28c7311c6edeed0ca0e497fd863389c3749c
rado196 <rado196b@gmail.com>
2022-06-20 19:27:20 +0400

    minimum update config for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: 'raspberry-client-app',
       exec_mode: 'cluster',
+      min_uptime: '15s',
       instances: 1,
       cwd: __dirname,
       script: './index.js',
