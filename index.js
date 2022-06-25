c2882d435826ed36e692f789c8115f64311b227d
rado196 <rado196b@gmail.com>
2022-06-25 19:26:51 +0400

    ....

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ function startClientApplication() {
   require('./src/lighting');
 }
 
-let intervalSteps = 30;
+let intervalSteps = 15;
 
 const interval = setInterval(function () {
   console.clear();
   console.log();
   console.log(`  Client app will start after ${intervalSteps} seconds, please wait ...`);
+  console.log('  We need to wait a bit till COM-port can respond us.');
+  console.log();
 
   if (--intervalSteps === 0) {
     console.clear();
