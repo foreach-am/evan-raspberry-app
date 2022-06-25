1c8373581aa64391d5255b4bf550700b93be3c76
rado196 <rado196b@gmail.com>
2022-06-25 19:22:41 +0400

    ....

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 global.ROOT_DIR = __dirname;
 
-function startApp() {
+function startClientApplication() {
   require('./src/configure');
   require('./src/client');
   require('./src/lighting');
 }
 
-setTimeout(function () {
-  startApp();
-}, 30_000);
+let intervalSteps = 30;
+
+const interval = setInterval(function () {
+  console.log(`  CLIENT APP WILL START IN ${intervalSteps} SECONDS.`);
+
+  if (--intervalSteps === 0) {
+    console.log();
+    console.log();
+    console.log();
+
+    clearInterval(interval);
+    startClientApplication();
+  }
+}, 1000);
