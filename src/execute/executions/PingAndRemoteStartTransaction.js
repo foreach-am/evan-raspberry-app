de459f7358d1247553adf08739e4701da1ea7d55
rado196 <rado196b@gmail.com>
2022-07-07 16:54:21 +0400

    RemoteStart - set transaction id

--- src/execute/executions/PingAndRemoteStartTransaction.js
+++ src/execute/executions/PingAndRemoteStartTransaction.js
@@ -4,9 +4,21 @@ const state = require('../../state');
 const ping = require('../../ping');
 
 module.exports = async function (parsedServerData) {
+  if (!parsedServerData.body || !parsedServerData.body.idTag) {
+    await ping.RemoteStartTransaction.execute(
+      parsedServerData.messageId,
+      parsedServerData.body.connectorId,
+      ping.RemoteStartTransaction.StatusEnum.REJECTED
+    );
+    return;
+  }
+
   state.state.plugs.idTags[parsedServerData.body.connectorId] = parsedServerData.body.idTag;
-  // state.state.plugs.transactionId[parsedServerData.body.connectorId] =
-  //   parsedServerData.body.chargingProfile.transactionId;
+
+  if (parsedServerData.body.chargingProfile && parsedServerData.body.chargingProfile.transactionId) {
+    state.state.plugs.transactionId[parsedServerData.body.connectorId] =
+      parsedServerData.body.chargingProfile.transactionId;
+  }
 
   await ping.RemoteStartTransaction.execute(
     parsedServerData.messageId,
