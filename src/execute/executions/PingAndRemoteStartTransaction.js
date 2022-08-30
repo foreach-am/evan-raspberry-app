a25a6b127476de9f813b1196d7d3fd78c102fbcb
rado196 <rado196b@gmail.com>
2022-08-30 22:26:07 +0400

    ...

--- src/execute/executions/PingAndRemoteStartTransaction.js
+++ src/execute/executions/PingAndRemoteStartTransaction.js
@@ -6,10 +6,10 @@ const ping = require('../../ping');
 module.exports = async function (parsedServerData) {
   // when idTag is invalid
   if (
-    !parsedServerData.body ||
-    typeof parsedServerData.body.idTag !== 'string' ||
-    !parsedServerData.body.idTag ||
-    parsedServerData.body.idTag.toLowerCase() === 'unknown'
+    !parsedServerData.body // ||
+    // typeof parsedServerData.body.idTag !== 'string' ||
+    // !parsedServerData.body.idTag ||
+    // parsedServerData.body.idTag.toLowerCase() === 'unknown'
   ) {
     await ping.RemoteStartTransaction.execute(
       parsedServerData.messageId,
@@ -19,21 +19,21 @@ module.exports = async function (parsedServerData) {
     return;
   }
 
-  // when transactionId is invalid
-  if (
-    !parsedServerData.body.transactionId &&
-    !parsedServerData.body.chargingProfile &&
-    !parsedServerData.body.chargingProfile.transactionId
-  ) {
-    await ping.RemoteStartTransaction.execute(
-      parsedServerData.messageId,
-      parsedServerData.body.connectorId,
-      ping.RemoteStartTransaction.StatusEnum.REJECTED
-    );
-    return;
-  }
+  // // when transactionId is invalid
+  // if (
+  //   !parsedServerData.body.transactionId &&
+  //   !parsedServerData.body.chargingProfile &&
+  //   !parsedServerData.body.chargingProfile.transactionId
+  // ) {
+  //   await ping.RemoteStartTransaction.execute(
+  //     parsedServerData.messageId,
+  //     parsedServerData.body.connectorId,
+  //     ping.RemoteStartTransaction.StatusEnum.REJECTED
+  //   );
+  //   return;
+  // }
 
-  state.state.plugs.idTags[parsedServerData.body.connectorId] = parsedServerData.body.idTag;
+  state.state.plugs.idTags[parsedServerData.body.connectorId] = parsedServerData.body.idTag || 'UNKNOWN';
 
   if (parsedServerData.body.chargingProfile && parsedServerData.body.chargingProfile.transactionId) {
     state.state.plugs.transactionId[parsedServerData.body.connectorId] =
