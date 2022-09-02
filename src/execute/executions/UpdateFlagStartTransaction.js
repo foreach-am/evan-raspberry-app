6a18161df19759c3338a98219e769d777fe5c34a
rado196 <rado196b@gmail.com>
2022-09-02 08:46:49 +0400

    some fixes

--- src/execute/executions/UpdateFlagStartTransaction.js
+++ src/execute/executions/UpdateFlagStartTransaction.js
@@ -1,6 +1,11 @@
 const state = require('../../state');
 
 module.exports = async function (parsedServerData, connectorId) {
+  if (!parsedServerData.body.transactionId) {
+    ComEmitter.plugStop(connectorId);
+    return;
+  }
+
   state.state.plugs.transactionId[connectorId] = parsedServerData.body.transactionId;
 
   state.state.plugs.startTransactionStatus[connectorId] =
