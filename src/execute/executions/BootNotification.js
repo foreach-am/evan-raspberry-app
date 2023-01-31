fdf060008636c048f92136baca4c34b30972e0ce
rado196 <rado196b@gmail.com>
2023-01-31 16:10:45 +0400

    Heartbeat - multiple requests - bug fix

--- src/execute/executions/BootNotification.js
+++ src/execute/executions/BootNotification.js
@@ -9,6 +9,7 @@ module.exports = async function (parsedServerData, onStart, onFinish) {
   state.state.common.bootNotStatus = parsedServerData.body.status;
   state.state.common.bootNotCurrentTime = parsedServerData.body.currentTime;
 
+  ping.Heartbeat.cleanup();
   await ping.Heartbeat.execute(uuid());
 
   if (typeof onStart === 'function') {
