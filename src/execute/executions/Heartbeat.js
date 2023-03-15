7cc31a84e984801b2072091d42f4cc13008fef92
rado196 <rado196b@gmail.com>
2023-03-15 13:14:33 +0400

    fixes

--- src/execute/executions/Heartbeat.js
+++ src/execute/executions/Heartbeat.js
@@ -3,6 +3,7 @@
 const { ComEmitter } = require('../../libraries/ComEmitter');
 const { Raspberry } = require('../../libraries/Raspberry');
 const { WebSocket } = require('../../libraries/WebSocket');
+const { PlugStateEnum } = require('../../libraries/PlugState');
 const state = require('../../state');
 
 module.exports = async function (parsedServerData) {
