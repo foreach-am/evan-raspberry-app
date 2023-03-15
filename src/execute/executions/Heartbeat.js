cc4c00c3deea9a1e6f4768c76052e8df30505659
rado196 <rado196b@gmail.com>
2023-03-15 12:57:10 +0400

    some fixes

--- src/execute/executions/Heartbeat.js
+++ src/execute/executions/Heartbeat.js
@@ -1,5 +1,21 @@
 /* eslint-disable no-unused-vars */
 
+const { ComEmitter } = require('../../libraries/ComEmitter');
+const { Raspberry } = require('../../libraries/Raspberry');
+const { WebSocket } = require('../../libraries/WebSocket');
+const state = require('../../state');
+
 module.exports = async function (parsedServerData) {
   // no action required
+
+  Raspberry.mapOnPlugs(async function (connectorId) {
+    if (
+      WebSocket.isConnected() &&
+      state.statistic.plugs.plugState[connectorId] ===
+        PlugStateEnum.PLUG_SOFT_LOCK &&
+      state.state.plugs.softLockDueConnectionLose[connectorId]
+    ) {
+      await ComEmitter.plugOn(connectorId);
+    }
+  });
 };
