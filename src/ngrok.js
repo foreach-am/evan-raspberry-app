f632a9c5bf4ea760cde926a99999006a1539290d
rado196 <rado196b@gmail.com>
2023-03-24 12:23:03 +0400

    ngrok test

--- src/ngrok.js
+++ src/ngrok.js
@@ -2,7 +2,7 @@ const ngrok = require('ngrok');
 
 (async function () {
   const url = await ngrok.connect({
-    authtoken: process.env.NGROK_AUTH_TOKEN,
+    // authtoken: process.env.NGROK_AUTH_TOKEN,
     proto: 'tcp',
     addr: 22,
     region: 'eu',
