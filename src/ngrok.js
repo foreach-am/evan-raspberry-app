5c91f79a8be0e36d21b9c743d23ba38e1792dc2c
rado196 <rado196b@gmail.com>
2023-03-24 12:42:56 +0400

    tunnel url

--- src/ngrok.js
+++ src/ngrok.js
@@ -1,14 +1,38 @@
 const ngrok = require('ngrok');
+const request = require('request');
+
+function sendTunnelUrl(url) {
+  const options = {
+    url: process.env.TUNNERL_UPDATE_URL,
+    json: true,
+    body: {
+      tunnelUrl: url,
+    },
+  };
+
+  request.post(options, (err, res, body) => {
+    if (err) {
+      return console.error(err);
+    }
+
+    console.log(`Status: ${res.statusCode}`);
+    console.log(body);
+  });
+}
 
 (async function () {
-  const url = await ngrok.connect({
-    // authtoken: process.env.NGROK_AUTH_TOKEN,
-    proto: 'tcp',
-    addr: 22,
-    region: 'eu',
-  });
+  try {
+    const url = await ngrok.connect({
+      authtoken: process.env.NGROK_AUTH_TOKEN,
+      proto: 'tcp',
+      addr: 22,
+      region: 'eu',
+    });
 
-  console.log();
-  console.log('ngrok url:',url);
-  console.log();
+    sendTunnelUrl(url);
+  } catch (e) {
+    console.error();
+    console.error('Failed tu update station tunnel URL:', e);
+    console.error();
+  }
 })();
