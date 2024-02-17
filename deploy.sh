571f015e07359fd672e312659e9b7c9df08434f5
rado196 <rado196b@gmail.com>
2024-02-17 17:58:29 +0400

    deploy command

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# go to directory
+cd /home/admin/raspberry-app
+
+# reset
+git reset --hard
+git checkout main
+git pull
+pm2 delete all
+pm2 save --force
+sudo systemctl stop configure-tunnel.service
+
+# clear
+rm -rf node_modules
+rm -rf logs/*.log
+rm data/macaddress.data
+
+# intsall
+npm install
+
+# start
+pm2 start ecosystem.config.js
+pm2 save --force
+sudo systemctl stop configure-tunnel.service
