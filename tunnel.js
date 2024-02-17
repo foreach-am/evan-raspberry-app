5f0a436405be1e14519c5a520a9887eba565cc31
rado196 <rado196b@gmail.com>
2024-02-17 13:46:21 +0400

    configure tunnel

--- tunnel.js
+++ tunnel.js
@@ -1,3 +1,5 @@
+const fs = require('node:fs');
+const os = require('node:os');
 const ngrok = require('ngrok');
 const axios = require('axios');
 require('./configure');
@@ -12,7 +14,7 @@ async function sendTunnelUrl(url) {
   }
 }
 
-async function connect() {
+async function connectTunnel() {
   try {
     const config = {
       authtoken: process.env.NGROK_AUTH_TOKEN,
@@ -32,11 +34,47 @@ async function connect() {
     console.error();
 
     setTimeout(function () {
-      connect();
+      connectTunnel();
     }, 5_000);
   }
 }
 
+async function configureYaml() {
+  const configFile = `${process.env.HOME}/.ngrok2/ngrok.yml`;
+
+  const configureKeyValue = function (key, value) {
+    if (!fs.existsSync()) {
+      fs.writeFileSync(configFile, '', 'utf8');
+    }
+
+    const oldContent = fs.readFileSync(configFile, 'utf8');
+    const oldLines = oldContent.split(os.EOL);
+
+    let found = false;
+    const newLines = oldLines.map(function (line) {
+      if (!line || !line.startsWith(`${key}:`)) {
+        return line;
+      }
+
+      found = true;
+      return `${key}: ${value}`;
+    });
+
+    if (!found) {
+      newLines.push(`${key}: ${value}`);
+    }
+
+    const newContent = newLines.join(os.EOL);
+    if (newContent !== oldContent) {
+      fs.writeFileSync(configFile, newContent, 'utf8');
+    }
+  };
+
+  configureKeyValue('authtoken', process.env.NGROK_AUTH_TOKEN);
+  configureKeyValue('version', '2');
+}
+
 (async function () {
-  await connect();
+  await configureYaml();
+  await connectTunnel();
 })();
