9c1be734fcdcffbb20ca47c69f18ee5dd7db9ec5
rado196 <rado196b@gmail.com>
2024-02-23 20:46:32 +0400

    jobs updated

--- jobs.js
+++ jobs.js
@@ -1,12 +1,16 @@
 const { execSync } = require('child_process');
 
+function runCommand(command) {
+  console.log(` >>> Running job: "${command}"`);
+  execSync(command, {
+    cwd: __dirname,
+  });
+}
+
 function register(command, minutes) {
   console.log(`Job registered: ${minutes} minutes - "${command}"`);
   setInterval(function () {
-    console.log(` >>> Running job: "${command}"`);
-    execSync(command, {
-      cwd: __dirname,
-    });
+    runCommand(command);
   }, minutes * 60 * 1000);
 }
 
@@ -15,6 +19,9 @@ process.on('uncaughtException', (err) => {
   console.error('Uncaught Exception:', err);
   console.error();
 });
+
+// execute on start
+runCommand('npm run tool:update-macaddress');
 
 // jobs ...
 register('npm run tool:update-source-code', 12 * 60); // twice daily
