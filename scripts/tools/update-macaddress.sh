36d2b2f03bd565b0bcb4522260e4d854adcc3b57
rado196 <rado196b@gmail.com>
2024-02-26 14:54:21 +0400

    macaddress script updated

--- scripts/tools/update-macaddress.sh
+++ scripts/tools/update-macaddress.sh
@@ -27,15 +27,15 @@ if [[ "$CURRENT_MACADDRESS" == "00:08:dc:01:02:03" ]]; then
   exit 0
 fi
 
-echo ">>>>>>>>> Putting network network: down"
-sudo ifconfig "$NETWORK_INTERFACE" "down"
-
 echo ">>>>>>>>> Generating new MAC Address ..."
 NEW_MACADDRES_VALUE="$(\
   printf '%02x:%02x:%02x:%02x:%02x:%02x\n' \
   "$[RANDOM%255]" "$[RANDOM%255]" "$[RANDOM%255]" \
   "$[RANDOM%255]" "$[RANDOM%255]" "$[RANDOM%255]" \
 )"
+
+echo ">>>>>>>>> Putting network network: down"
+sudo ifconfig "$NETWORK_INTERFACE" "down"
 
 echo ">>>>>>>>> Setting new MAC Address ..."
 sudo ifconfig "$NETWORK_INTERFACE" hw ether "$NEW_MACADDRES_VALUE"
