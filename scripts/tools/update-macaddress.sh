9af57d0f3faf88c0bb8e176756daa5e01b376478
rado196 <rado196b@gmail.com>
2024-04-01 11:32:33 +0400

    debugging

--- scripts/tools/update-macaddress.sh
+++ scripts/tools/update-macaddress.sh
@@ -17,33 +17,13 @@ source "$(dirname "$0")/../includes.sh"
 MACADDRESS_STORE_FILE="$ROOT_DIR/data/macaddress.data"
 DEFAULT_MACADDRESS="00:08:dc:01:02:03"
 
-function generate_macaddress() {
-  local CURRENT_MACADDRESS="$(get_macaddress)"
-  echo "CURRENT_MACADDRESS = $CURRENT_MACADDRESS"
-  echo "DEFAULT_MACADDRESS = $DEFAULT_MACADDRESS"
-
-  if [[ -f "$MACADDRESS_STORE_FILE" ]]; then
-    CURRENT_MACADDRESS="$(cat "$MACADDRESS_STORE_FILE")"
-  fi
-  echo "CURRENT_MACADDRESS_FILE = $CURRENT_MACADDRESS"
-
-  if [[ \
-    "$CURRENT_MACADDRESS" == "$DEFAULT_MACADDRESS" || \
-    "$CURRENT_MACADDRESS" == "" \
-  ]]; then
-    CURRENT_MACADDRESS="$(\
-      printf '%02x:%02x:%02x:%02x:%02x:%02x\n' \
-        "$[RANDOM%255]" "$[RANDOM%255]" "$[RANDOM%255]" \
-        "$[RANDOM%255]" "$[RANDOM%255]" "$[RANDOM%255]" \
-    )"
-  fi
-
-  echo "CURRENT_MACADDRESS_FINAL = $CURRENT_MACADDRESS"
-  echo "$CURRENT_MACADDRESS"
-}
 function store_macaddress() {
+  local CURRENT_MACADDRESS="$1"
+
   sudo rm -rf "$MACADDRESS_STORE_FILE"
-  echo "$1" > "$MACADDRESS_STORE_FILE"
+  echo "$CURRENT_MACADDRESS" > "$MACADDRESS_STORE_FILE"
+
+  echo ">>>>>>>>> MAC-Address value stored in file: $CURRENT_MACADDRESS"
 }
 
 ## ----------------------------------------------------------------------------------
@@ -56,20 +36,38 @@ fi
 ## ----------------------------------------------------------------------------------
 ## update macaddress
 CURRENT_MACADDRESS="$(get_macaddress)"
-if [[ "$CURRENT_MACADDRESS" != "$DEFAULT_MACADDRESS" ]]; then
+if [[ \
+  "$CURRENT_MACADDRESS" != "$DEFAULT_MACADDRESS" && \
+  "$CURRENT_MACADDRESS" != "" \ &&
+  "$CURRENT_MACADDRESS" =~ "^([a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2}$" \
+]]; then
+  echo ">>>>>>>>> An valid MAC-Address already updated, value: $CURRENT_MACADDRESS"
   store_macaddress "$CURRENT_MACADDRESS"
-  echo ">>>>>>>>> MAC Address already updated, value: $CURRENT_MACADDRESS"
   exit 0
 fi
 
-echo ">>>>>>>>> Generating new MAC Address ..."
-NEW_MACADDRES_VALUE="$(generate_macaddress)"
-echo ">>>>>>>>> New Mac Address value will be \"$NEW_MACADDRES_VALUE\""
+NEW_MACADDRES_VALUE=""
+if [[ -f "$MACADDRESS_STORE_FILE" ]]; then
+  echo ">>>>>>>>> Getting previously saved MAC-Address form file ..."
+  NEW_MACADDRES_VALUE="$(cat "$MACADDRESS_STORE_FILE")"
+fi
+if [[ \
+  "$CURRENT_MACADDRESS" == "$DEFAULT_MACADDRESS" || \
+  "$CURRENT_MACADDRESS" == "" \
+]]; then
+  echo ">>>>>>>>> Generating new MAC-Address ..."
+  NEW_MACADDRES_VALUE="$(\
+    printf '%02x:%02x:%02x:%02x:%02x:%02x\n' \
+      "$[RANDOM%255]" "$[RANDOM%255]" "$[RANDOM%255]" \
+      "$[RANDOM%255]" "$[RANDOM%255]" "$[RANDOM%255]" \
+  )"
+fi
+echo ">>>>>>>>> MAC-Address value will be \"$NEW_MACADDRES_VALUE\""
 
 echo ">>>>>>>>> Putting network network: down"
 sudo ifconfig "$NETWORK_INTERFACE" "down"
 
-echo ">>>>>>>>> Setting new MAC Address ..."
+echo ">>>>>>>>> Setting new MAC-Address ..."
 sudo ifconfig "$NETWORK_INTERFACE" hw ether "$NEW_MACADDRES_VALUE"
 
 echo ">>>>>>>>> Putting network network: up"
