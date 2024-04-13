c2d90bc84a1bd73197211112f5f71bd6a72d9d2c
rado196 <rado196b@gmail.com>
2024-04-13 14:49:13 +0400

    testing

--- src/tunnel/localtunnel.js
+++ src/tunnel/localtunnel.js
@@ -1,10 +1,16 @@
 const localtunnel = require('localtunnel');
 
+function randomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 async function connectTunnel(onTerminated) {
   const tunnel = await localtunnel({
     port: 22,
     allow_invalid_cert: true,
-    subdomain: 'embers',
+    subdomain: 'eevan-' + randomNumber(100000, 999999),
   });
 
   tunnel.on('close', onTerminated);
